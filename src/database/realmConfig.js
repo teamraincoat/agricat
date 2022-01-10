ef14531a770719c2d58f33c05d9567480a1355e5
Tushali <53653089+tushali024@users.noreply.github.com>
2022-01-10 18:37:33 +0530

    update sync config

--- src/database/realmConfig.js
+++ src/database/realmConfig.js
@@ -3,11 +3,15 @@ import {UserSchema, User_imagesSchema} from '../../schemas';
 
 export const app = new Realm.App({id: 'enrollmenttest-acysm', timeout: 10000});
 const getRealm = async (userId) => {
+  const OpenRealmBehaviorConfiguration = {
+    type: 'openImmediately',
+  };
   const configuration = {
     schema: [UserSchema, User_imagesSchema],
     sync: {
       user: app.currentUser,
-      partitionValue:  app.currentUser ? app.currentUser.id : userId,  //app.currentUser.id, // should be userId(Unique) so it can manage particular user related documents in DB by userId
+      partitionValue: app.currentUser ? app.currentUser.id : userId, //app.currentUser.id,
+      newRealmFileBehavior: OpenRealmBehaviorConfiguration,
     },
   };
 
