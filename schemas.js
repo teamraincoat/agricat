4daa86b9719d3314af4ac1b6ba5d484689032ffb
Tushali <tushali024@gmail.com>
2022-01-28 12:20:02 +0530

    updated schema as per realm

--- schemas.js
+++ schemas.js
@@ -1,76 +1,86 @@
 const OrganizerSchema = {
-    name: 'Organizer',
-    properties: {
-      _id: 'objectId',
-      _partition: 'string',
-      insurer: 'Insurer',
-      name: 'string',
-      status: 'string',
-    },
-    primaryKey: '_id',
-  };
+  name: 'Organizer',
+  properties: {
+    _id: 'objectId',
+    _partition: 'string',
+    insurer: 'Insurer',
+    name: 'string',
+    status: 'string',
+  },
+  primaryKey: '_id',
+};
+
 const UserSchema = {
-    name: 'User',
-    properties: {
-      _id: 'string',
-      _partition: 'string',
-      email: 'string',
-      memberOf: 'User_memberOf[]',
-      name: 'string',
-    },
-    primaryKey: '_id',
-  };
+  name: 'User',
+  properties: {
+    _id: 'string',
+    _partition: 'string',
+    email: 'string',
+    memberOf: 'User_memberOf[]',
+    name: 'string',
+  },
+  primaryKey: '_id',
+};
+
 const User_memberOfSchema = {
-    name: 'User_memberOf',
-    embedded: true,
-    properties: {
-      name: 'string?',
-      partition: 'string?',
-    },
-  };
+  name: 'User_memberOf',
+  embedded: true,
+  properties: {
+    name: 'string?',
+    partition: 'string?',
+  },
+};
+
 const CampaignSchema = {
-    name: 'Campaign',
-    properties: {
-      _id: 'objectId',
-      _partition: 'string',
-      description: 'string?',
-      encryptionKey: 'string',
-      endTime: 'string?',
-      name: 'string',
-      organizer: 'Organizer',
-      startTime: 'string?',
-      status: 'string',
-    },
-    primaryKey: '_id',
-  };
-  const EnrollmentSchema = {
+  name: 'Campaign',
+  properties: {
+    _id: 'objectId',
+    _partition: 'string',
+    description: 'string?',
+    encryptionKey: 'string',
+    endTime: 'string',
+    name: 'string',
+    organizer: 'Organizer',
+    startTime: 'string',
+    status: 'string',
+  },
+  primaryKey: '_id',
+};
+
+const EnrollmentSchema = {
     name: 'Enrollment',
     properties: {
-      _id: 'objectId',
-      _partition: 'string',
-      address1: 'string',
-      applicationTime: 'date?',
-      campaign: 'Campaign?',
-      coveredArea: 'string?',
-      crop: 'string',
-      cropCycle: 'string?',
-      cropType: 'string?',
-      dob: 'date',
-      firstName: 'string',
-      gender: 'string',
-      geoJson: 'string?',
-      images: 'Enrollment_images[]',
-      lastName: 'string',
-      locality: 'string?',
-      mobilePhone: 'string',
-      municipality: 'string',
-      status: 'string?',
-      surName: 'string?',
-    },
-    primaryKey: '_id',
-  };
+    _id: 'objectId',
+    _partition: 'string',
+    addressLine: 'string?',
+    adminArea: 'string',
+    adminAreaId: 'string',
+    applicationTime: 'date?',
+    campaign: 'Campaign',
+    coveredAreaHa: 'decimal128?',
+    crop: 'string',
+    cropId: 'string?',
+    cropType: 'string?',
+    dob: 'date',
+    firstName: 'string',
+    gender: 'string',
+    geoJson: 'string?',
+    images: 'Enrollment_images[]',
+    lastName: 'string',
+    locality: 'string',
+    localityId: 'string',
+    mobilePhone: 'string?',
+    payoutMethod: 'string?',
+    payoutMethodId: 'string?',
+    status: 'string?',
+    subLocality: 'string',
+    subLocalityId: 'string',
+    surName: 'string?',
+  },
+  primaryKey: '_id',
+};;
 
-  const Enrollment_imagesSchema = {
+const Enrollment_imagesSchema = {
     name: 'Enrollment_images',
     embedded: true,
     properties: {
@@ -79,8 +89,9 @@ const CampaignSchema = {
       type: 'string?',
       uri: 'string?',
     },
-  };
-  const InsurerSchema = {
+};
+
+const InsurerSchema = {
     name: 'Insurer',
     properties: {
       _id: 'objectId',
@@ -90,15 +101,17 @@ const CampaignSchema = {
       status: 'string',
     },
     primaryKey: '_id',
-  };
-  const LocationSchema = {
-    name: 'Location',
-    embedded: true,
-    properties: {
-      name: 'string?',
-      partition: 'string?',
-    },
-  };
+};
+
+const LocationSchema = {
+  name: 'Location',
+  embedded: true,
+  properties: {
+    name: 'string?',
+    partition: 'string?',
+  },
+};
+
 export {
     OrganizerSchema,
     UserSchema,
