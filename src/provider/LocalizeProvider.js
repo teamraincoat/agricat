db190fba1173fabfacc5a2ed099c75c4b81eef7d
Joel Martínez <jmgalanes@gmail.com>
2022-04-22 19:04:25 -0400

    Squashed commit of the following:
    commit 8f84696cd064b9718ef91924029e8f1ec97c290b
    Author: Joel Martínez <jmgalanes@gmail.com>
    Date:   Fri Apr 22 13:24:17 2022 -0400
    
        chore: Bump react-native and realm to stable latest
    
    commit 32de59e84e38865adab8d8f61b309428b0348e9c
    Author: Joel Martínez <jmgalanes@gmail.com>
    Date:   Fri Apr 22 02:28:25 2022 -0400
    
        chore: Clean up more code
    
    commit 293682ab12179bf7dc7c0b80d412ad8646f82b56
    Author: Joel Martínez <jmgalanes@gmail.com>
    Date:   Fri Apr 22 01:54:57 2022 -0400
    
        chore: Clean up localization provider

--- src/provider/LocalizeProvider.js
+++ src/provider/LocalizeProvider.js
@@ -1,7 +1,6 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import LocalizedStrings from 'react-native-localization';
-import * as RNLocalize from 'react-native-localize';
 
 import en from '../localization/en.json';
 import es from '../localization/es.json';
@@ -31,17 +30,6 @@ export const LocalizeProvider = ({ children }) => {
     );
 
     if (!currentLanguage) {
-      // let localeCode = Constants.DEFAULT_LANGUAGE;
-      // const supportedLocaleCodes = translations.getAvailableLanguages();
-      // const phoneLocaleCodes = RNLocalize.getLocales().map(
-      //   (locale) => locale.languageCode,
-      // );
-      // phoneLocaleCodes.some((code) => {
-      //   if (supportedLocaleCodes.includes(code)) {
-      //     localeCode = code;
-      //     return true;
-      //   }
-      // });
       setLanguage(Constants.DEFAULT_LANGUAGE);
     } else {
       setLanguage(currentLanguage);
